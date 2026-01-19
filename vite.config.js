72b6607e1864f52fd82c44eb213896f8f1f4766a
Kristian Duda <kristian.duda@extropy.sk>
2026-01-19 21:17:39 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import swc from 'vite-plugin-swc-transform'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import { name } from './package.json'
+
+const formattedName = name.match(/[^/]+$/)?.[0] ?? name
+
+export default defineConfig({
+  plugins: [
+    dts(),
+    swc({
+      swcOptions: {
+        jsc: {
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+          },
+        },
+      },
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: formattedName,
+    },
+    rollupOptions: {
+      external: ['@nestjs/common'],
+    },
+  },
+})
