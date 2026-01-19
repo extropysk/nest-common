fd2e4c7c57a7dad93f832dff7c9c5a0b30173100
Kristian Duda <kristian.duda@extropy.sk>
2026-01-19 22:37:32 +0100

    feat: storage config

--- src/modules/storage.module.ts
+++ src/modules/storage.module.ts
@@ -1,5 +1,11 @@
 import { DynamicModule, Module, Type } from '@nestjs/common'
 
+export const STORAGE_CONFIG = 'storage_config'
+
+export interface StorageConfig {
+  collectionName?: string
+}
+
 export abstract class IStorageService {
   abstract set<T>(key: string, value: T, exp?: number): Promise<void>
   abstract get<T>(key: string): Promise<T | null>
@@ -8,6 +14,14 @@ export abstract class IStorageService {
 
 interface Config {
   useClass: Type<IStorageService>
+  config?: StorageConfig
+}
+
+interface AsyncConfig {
+  useClass: Type<IStorageService>
+  useFactory: (...args: unknown[]) => StorageConfig | Promise<StorageConfig>
+  inject?: any[]
+  imports?: any[]
 }
 
 @Module({})
@@ -18,11 +32,35 @@ export class StorageModule {
       global: true,
       providers: [
         {
+          provide: STORAGE_CONFIG,
+          useValue: options.config ?? {},
+        },
+        {
           provide: IStorageService,
           useClass: options.useClass,
         },
       ],
-      exports: [IStorageService],
+      exports: [IStorageService, STORAGE_CONFIG],
+    }
+  }
+
+  static forRootAsync(options: AsyncConfig): DynamicModule {
+    return {
+      module: StorageModule,
+      global: true,
+      imports: options?.imports || [],
+      providers: [
+        {
+          provide: STORAGE_CONFIG,
+          useFactory: options.useFactory,
+          inject: options.inject ?? [],
+        },
+        {
+          provide: IStorageService,
+          useClass: options.useClass,
+        },
+      ],
+      exports: [IStorageService, STORAGE_CONFIG],
     }
   }
 }
